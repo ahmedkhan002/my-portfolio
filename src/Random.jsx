4afac82ace86b19ca77613e82216f6c3053c88e8
Ahmed Khan <ahmedkhan81x@gmail.com>
2025-06-13 20:36:41 +0500

    'layout'

---
+++ src/Random.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import image1 from  './images/image1.jpg'
+import image2 from  './images/image2.jpg'
+import image3 from  './images/image3.avif'
+import image4 from  './images/image4.jpg'
+
+
+const random = () => {
+  return (
+    <>
+    <div className='w-full h-full sticky top-0'>
+        <img className='w-full h-full' src={image1} alt="" />
+    </div>
+    <div className='w-full h-full sticky top-0'>
+        <img className='w-full h-full' src={image2} alt="" />
+    </div>
+    <div className='w-full h-full sticky top-0'>
+        <img className='w-full h-full' src={image3} alt="" />
+    </div>
+    <div className='w-full h-full sticky top-0'>
+        <img className='w-full h-full' src={image4} alt="" />
+    </div>
+    </>
+  )
+}
+
+export default random
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <header className='flex flex-col gap-10 items-center justify-center h-full'>
+        <div>
+            <p>Home</p>
+        </div>
+        <div>
+            <p>About</p>
+        </div>
+        <div>
+            <p>Projects</p>
+        </div>
+        <div>
+            <p>Services</p>
+        </div>
+        <div>
+            <p>Contact</p>
+        </div>
+    </header>
+    </>
+  )
+}
+
+export default Navbar
