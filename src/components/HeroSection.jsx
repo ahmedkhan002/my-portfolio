aa484bbf3a99c5c4af026352ceba5320a5b4bb0a
Ahmed Khan <ahmedkhan81x@gmail.com>
2025-06-15 02:10:11 +0500

    .

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const HeroSection = (id) => {
+const HeroSection = () => {
   return (
     <>
-        <section id={'hero'}>
+        <section>
 
         </section>
     </>
